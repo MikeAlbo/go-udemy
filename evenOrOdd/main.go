970ae99958d826e4ceb198d26094e46231f07170
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-08-27 12:29:44 -0500

    complete even-odd-project

--- evenOrOdd/main.go
+++ evenOrOdd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+// create a main function
+// create a slice of ints from 0 to 10
+// iterate thorugh a slice with a for loop
+// 		if the value is even, print out "is even"
+//		if the value is odd, print out "is odd"
+// example "0 is even"
+
+func main() {
+	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, int := range ints {
+		if int%2 == 0 {
+			fmt.Println(strconv.Itoa(int) + " is even")
+		} else {
+			fmt.Println(strconv.Itoa(int) + " is odd")
+		}
+	}
 }
