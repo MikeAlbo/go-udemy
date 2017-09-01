f77ae928c75c0c90425d1c358aa8778668ce7e4f
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-08-31 20:18:19 -0500

    begin http project

--- interface/main.go
+++ interface/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// very custom logic for generating a english greeting
+	return "hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// custom logic for generating spanish greeting
+	return "Hola!"
 }
